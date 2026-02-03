22125340a43e4ff786a7a887e5f8341650100e9b
TheGeneric <124943354+TheGeneric10@users.noreply.github.com>
2026-02-03 10:06:24 -0500

    Add online mode UI and gameplay updates

--- js/game.env.js
+++ js/game.env.js
@@ -1,8 +1,8 @@
 /* Last Updated 1/5/2026 at 2:42 AM EDT */
 
 window.GAME_ENV = {
-  version: "1.0.0",
+  version: "1.1.0",
   name: "TicTacToe Arena",
   description: "Tap a cell to place your mark. Get 3 in a row to win!",
-  info: "This game will be getting frequent updates overtime and seasonal updates."
+  info: "Version 1.1.0 adds Online Mode (beta), improved animations, and gameplay polish."
 };
